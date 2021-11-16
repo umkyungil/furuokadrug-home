333fc4face444159c252a51bc172430cc9977805
um.kyungil <umkyungil@gmail.com>
2021-11-16 23:40:48 +0900

    Updates

---
+++ precache-manifest.33399bef733f1be4026302650d87c700.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51c71cd6b03850e5f85fabc41617c1c",
+    "url": "/furuoka-drug-master/index.html"
+  },
+  {
+    "revision": "9a18a5b9f7a8ebdc5d6c",
+    "url": "/furuoka-drug-master/static/css/2.051905ae.chunk.css"
+  },
+  {
+    "revision": "96c673e39de1421ff2e0",
+    "url": "/furuoka-drug-master/static/css/main.d6c2332f.chunk.css"
+  },
+  {
+    "revision": "9a18a5b9f7a8ebdc5d6c",
+    "url": "/furuoka-drug-master/static/js/2.f23e974a.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/furuoka-drug-master/static/js/2.f23e974a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c673e39de1421ff2e0",
+    "url": "/furuoka-drug-master/static/js/main.e0659356.chunk.js"
+  },
+  {
+    "revision": "ccc36c82d5d215fe9a20",
+    "url": "/furuoka-drug-master/static/js/runtime-main.3ddaadf3.js"
+  }
+]);
